f3415184754ca75ca389a8359d090a76a1d8a06f
sjklan4 <dpeltms4@gmail.com>
2023-09-21 12:43:04 +0900

    0921컨트롤러 추가

--- ssermincar/app/Http/Controllers/MainhomeController.php
+++ ssermincar/app/Http/Controllers/MainhomeController.php
@@ -1,7 +1,8 @@
 <?php
-
+//필요 테이블 : 회원(구매,판매), 차량정보(차량 정보 + 이미지 경로저장컬럼), 
 namespace App\Http\Controllers;
 
+use App\Http\Middleware\RedirectIfAuthenticated;
 use Illuminate\Http\Request;
 
 class MainhomeController extends Controller
@@ -9,4 +10,24 @@ class MainhomeController extends Controller
     public function home(){
         return view('home');
     }
+
+
+    public function imgEdit(Request $req){
+        if ($req->hasFile('carimg')) {
+            $img = $req->file('carimg');
+            $fileName = $req->file('carimg')->getClientOriginalName();
+
+            $img->move(public_path('carimg'), $fileName);
+
+            $imgePath = 'carimg/' . $carimg;
+
+            $carlist->d_img_path = $imgePath;
+            $carlist->save();
+
+        }
+
+        return redirect()->route('home');
+    }
 }
+
+

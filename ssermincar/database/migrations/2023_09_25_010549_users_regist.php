bdd67aa9d8a957a3ba6b4357ebee4ba5dcf21420
sjklan4 <dpeltms4@gmail.com>
2023-09-25 22:21:29 +0900

    0925테이블작성

--- ssermincar/database/migrations/2023_09_25_010549_users_regist.php
+++ ssermincar/database/migrations/2023_09_25_010549_users_regist.php
@@ -13,9 +13,16 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('users_regist', function (Blueprint $table) {
             $table->integer('Certifi_num')->autoincrement();
-            
+            $table->integer('Seller_num')->nullable(); // 판매자 일련 번호
+            $table->foreign('Seller_num')->references('Seller_num')->on('seller_info'); //외래키 참조-판매자 테이블
+            $table->integer('User_num')->nullable(); // 일반사용자 일련 번호
+            $table->integer('User_num')->references('User_num')->on('user_info'); // 외래키 참조 - 일반유저 테이블
+            $table->string('Certifi_numbering',1000);
+            $table->timestamp('Send_time');//인증번호 발송시간
+            $table->timestamp('Destory_time'); // 파기 시간
+
         });
     }
 
--- ssermincar/database/migrations/2023_09_25_010804_car_checkinfo.php
+++ ssermincar/database/migrations/2023_09_25_010804_car_checkinfo.php
@@ -13,8 +13,16 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
-
+        Schema::create('car_checkinfo', function (Blueprint $table) {
+            $table->integer('Ch_num')->autoincrement();
+            $table->integer('Car_num');
+            $table->integer('Seller_num')->nullable(); // 판매자 일련 번호
+            $table->foreign('Seller_num')->references('Seller_num')->on('seller_info'); //외래키 참조-판매자 테이블
+            $table->integer('User_num')->nullable(); // 일반사용자 일련 번호
+            $table->integer('User_num')->references('User_num')->on('user_info'); // 외래키 참조 - 일반유저 테이블
+            $table->char('Check_cmp_num',1);
+            $table->string('Check_link',1000);
+            $table->date('Check_data');
         });
     }
 

0482c8b362dee25b0ff3bbcfc91eee9b5266eeb3
sjklan4 <dpeltms4@gmail.com>
2023-10-10 13:19:10 +0900

    1010write

--- ssermincar/database/migrations/2023_09_25_010359_car_info.php
+++ ssermincar/database/migrations/2023_09_25_010359_car_info.php
@@ -15,8 +15,6 @@ return new class extends Migration
     {
         Schema::create('car_info', function (Blueprint $table) {
             $table->integer('Car_num')->autoIncrement()->primary(); // 차량 일련번호
-            $table->integer('Seller_num')->nullable(); // 판매자 일련 번호
-            $table->integer('User_num')->nullable(); // 일반사용자 일련 번호
             $table->string('Car_identNum',100); //차대번호
             $table->string('Car_Registnum',500); // 차량 등록번호 - 차번호
             $table->string('Car_Name',1000); // 차량 이름
@@ -29,6 +27,11 @@ return new class extends Migration
             $table->string('Car_imgadd',1000)->nullable(); // 이미지 저장 경로 저장 컬럼
             $table->char('Check_exgist',1)->default(0); // 진단검사 유무 - 0은 무 1은 진행완료
             $table->integer('Get_change_cnt'); // 소유주 변경 횟수
+            $table->integer('Seller_num')->nullable(); // 판매자 일련 번호
+            $table->integer('User_num')->nullable(); // 일반사용자 일련 번호
+
+            $table->string('Car_color',500);
+            $table->integer('Car_value');
             $table->string('Car_address',1000); // 차량이 있는 위치 - 판매장 주소 또는 차량 위치
 
         });
--- ssermincar/database/migrations/2023_09_25_010441_seller_info.php
+++ ssermincar/database/migrations/2023_09_25_010441_seller_info.php
@@ -19,13 +19,12 @@ return new class extends Migration
             $table->string('password',1000); // 판매자 계정 비번
             $table->string('Seller_phone_num',1000); // 판매자 연락처
             $table->string('Seller_call_num',1000)->nullable(); //판매자 연락처2
-            $table->string('Sell_addres',1000); // 판매자 주소
             $table->string('Seller_ID_num',1000); // 판매 허가증 번호 - 중고차 판매자 등록번호
             $table->string('Buss_num',1000); //사업자 번호
             $table->integer('RegistCar_num')->nullable(); //등록된 판매차량 수
-            $table->integer('Contract_num')->nullable(); // 계약 건수 - 판매 건수
-
             $table->string('Remember_regi',1000); // 가입시 인증여부 - 가입자 허가 여부
+            $table->integer('Contract_num')->nullable(); // 계약 건수 - 판매 건수
+            $table->string('Sell_addres',1000); // 판매자 주소
             $table->delete('delflg');
         });
     }

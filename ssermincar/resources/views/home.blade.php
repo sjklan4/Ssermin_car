02de21777e8023be7004875986d482fb938f1ce9
sjklan4 <dpeltms4@gmail.com>
2023-11-27 00:07:44 +0900

    1127

--- ssermincar/resources/views/home.blade.php
+++ ssermincar/resources/views/home.blade.php
@@ -1,6 +1,28 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('css/home.css')}}">
+</head>
 
-@section('homecontents')
-    <div>
-        <a href="{{route('usernomal.regist')}}">회원가입</a>
+<body>
+    <div class="container">
+        <div class="header">
+            @include('layout.header')
+        </div>
+        <hr>
+
+        <div class="mainhomebody">
+            <p>여기 메인페이지</p>
+        </div>
+        
+        <hr>
+        <div class="footer">
+            @include('layout.footer')
+        </div>
     </div>
-@endsection
+</body>
+</html>
--- ssermincar/resources/views/layout/header.blade.php
+++ ssermincar/resources/views/layout/header.blade.php
@@ -1,6 +1,15 @@
-
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('css/header.css')}}">
+</head>
+<body>
     <ul class="menu_list">
-        <li class="menu_name"><h1><a href="{{route('home')}}">로고위치</a></h1></li>
+        <li class="menu_name"><a href="{{route('home')}}">로고위치</a></li>
         <li class="menu_name">최저 도전!</li>
         <li class="menu_name">이차어때</li>
         <li class="menu_name">국산</li>
@@ -13,3 +22,5 @@
         <li class="menu_name">시세</li>
         <li class="menu_name">구매/판매지원</li>
     </ul>
+</body>
+</html>
